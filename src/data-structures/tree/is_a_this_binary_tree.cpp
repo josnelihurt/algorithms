a04691781e8f88bc73d8d1e0fcf96232f93d070c
Josnelihurt <josnelihurt@gmail.com>
2021-06-05 15:55:21 -0500

    more solved hacker rank problems

---
+++ src/data-structures/tree/is_a_this_binary_tree.cpp
@@ -0,0 +1,49 @@
+/*
+https://www.hackerrank.com/challenges/is-binary-search-tree/problem
+*/
+
+//This file problem was solved without a tdd approach
+//maybe I will be back to create at least some test cases :)
+
+/* Hidden stub code will pass a root argument to the function below. Complete the function to solve the challenge. Hint: you may want to write one or more helper functions.  
+
+The Node struct is defined as follows:
+	struct Node {
+		int data;
+		Node* left;
+		Node* right;
+	}
+*/
+    bool checkLessThan(Node* root, int value){
+        if(root==nullptr) return true;
+        if(checkLessThan(root->left,value)
+          &&checkLessThan(root->right,value)
+          &&root->data<value)
+            return true;
+        else
+            return false;
+            
+    }
+    bool checkMoreThan(Node* root, int value){
+        if(root==nullptr) return true;
+        if(checkMoreThan(root->left,value)
+          &&checkMoreThan(root->right,value)
+          &&root->data>value)
+            return true;
+        else
+            return false;
+            
+        
+    }
+	bool checkBST(Node* root) {
+		if(root==nullptr) return true;
+        
+        if(checkLessThan(root->left, root->data) 
+           && checkMoreThan(root->right,root->data)
+           && checkBST(root->left)
+           && checkBST(root->right)){
+            return true;
+        } else{
+            return false;
+        }        
+	}
