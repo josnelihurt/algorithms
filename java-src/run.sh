0b58d443bb42e997f289bbe1fb83ec1e670d8a5b
Josnelihurt <josnelihurt@gmail.com>
2021-06-11 19:15:08 -0500

    fixing docker

--- java-src/run.sh
+++ java-src/run.sh
@@ -1,5 +1,5 @@
-export JAVA_HOME=/usr/lib/jvm/default-java
-export M2_HOME=/opt/maven
-export MAVEN_HOME=/opt/maven
-export PATH=${M2_HOME}/bin:${PATH}
+JAVA_HOME=/usr/lib/jvm/default-java
+M2_HOME=/opt/maven
+MAVEN_HOME=/opt/maven
+PATH=${M2_HOME}/bin:${PATH}
 mvn package
